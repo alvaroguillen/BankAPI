112ee198eebb95f8505a336b4f5901eebaf6f99b
Alvaro Guillen <guillenalvarofabian@gmail.com>
2025-07-15 19:49:08 -0300

    Agregar archivos de proyecto.

---
+++ Data/BankModels/Cuentum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BankAPI.Data.BankModels;
+
+public partial class Cuentum
+{
+    public int Id { get; set; }
+
+    public int TipoCuentaId { get; set; }
+
+    public int? ClienteId { get; set; }
+
+    public decimal Saldo { get; set; }
+
+    public DateTime FechaRegistro { get; set; }
+
+    public virtual Cliente? Cliente { get; set; }
+
+    public virtual TipoCuentum TipoCuenta { get; set; } = null!;
+
+    public virtual ICollection<TransaccionBancarium> TransaccionBancaria { get; set; } = new List<TransaccionBancarium>();
+}
