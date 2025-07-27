dd7d7aa28f636d9846dfeca11dc8d480f6e98b9c
Alvaro Guillen <guillenalvarofabian@gmail.com>
2025-07-27 11:43:02 -0300

    Agregado ClienteController con endpoints explícitos (crear, obtenerTodo, editar, eliminar)

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace BankAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class ClienteController : ControllerBase
     {
         private readonly ClienteServicio _clienteServicio;
