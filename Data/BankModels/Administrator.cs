27e8a390b1dbb12bacbc6c42ca18209cc19ee8e7
Alvaro Guillen <guillenalvarofabian@gmail.com>
2025-07-28 20:08:46 -0300

    Agrega el nuevo Modelo Administrador en la carpeta Data/BankModels

---
+++ Data/BankModels/Administrator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BankAPI.Data.BankModels;
+
+public partial class Administrator
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string PhoneNumber { get; set; } = null!;
+
+    public string? Email { get; set; }
+
+    public string PwdHash { get; set; } = null!;
+
+    public string AdminType { get; set; } = null!;
+
+    public DateTime RegDate { get; set; }
+}
