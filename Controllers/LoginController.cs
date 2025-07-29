1391914e6aabe1e8592c6ed4964e99c8675f87d6
Alvaro Guillen <guillenalvarofabian@gmail.com>
2025-07-28 22:17:05 -0300

    Implementar autenticación con JWT: LoginController, LoginServicio y AdminDto

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using BankAPI.Data.DTOs;
+using BankAPI.Data.BankModels;
+using BankAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly LoginServicio loginServicio;
+        public LoginController(LoginServicio loginServicio)
+        {
+            this.loginServicio = loginServicio;
+        }
+
+        [HttpPost("autenticacion")]
+        public async Task<IActionResult> Login(AdminDto adminDto)
+        {
+            var admin = await loginServicio.ObtenerAdmin(adminDto);
+
+            if(admin is null)
+            {
+                return BadRequest(new { message = "Credenciales invalidas."});
+            }
+
+            return Ok(new {token = "algun valor"});
+        }
+    }
+}
---
+++ Data/DTOs/AdminDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BankAPI.Data.DTOs
+{
+    public class AdminDto
+    {
+        public string Email { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+    }
+}
